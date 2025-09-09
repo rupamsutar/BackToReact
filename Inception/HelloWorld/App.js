942baf83d623d2a9b881bb484192e63a3e49abde
rupamsutar <rupamsutar281998@gmail.com>
2025-09-09 23:57:18 +0530

    Entering into the JSX.

--- Inception/HelloWorld/App.js
+++ Inception/HelloWorld/App.js
@@ -5,5 +5,7 @@ const heading = React.createElement("h1", {
     id: "heading"
 }, "Namaste React ! ❤️");
 
+const jsxHeading = <h1>Namaste React using JSX !❤️</h1>
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
